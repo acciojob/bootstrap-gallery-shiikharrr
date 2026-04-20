db479674ddf7f5751d4b48caca299bff6a6138fe
Shikhar Gupta <69209995+shiikharrr@users.noreply.github.com>
2026-04-20 11:47:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const galleryImages = document.querySelectorAll(".gallery-img");
+const modalImage = document.getElementById("modalImage");
+
+galleryImages.forEach((img) => {
+  img.addEventListener("click", () => {
+    modalImage.src = img.src;
+    modalImage.alt = img.alt;
+  });
+});
