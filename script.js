f4f8134a401ba4080a6583065746c008a1de1c51
Shikhar Gupta <69209995+shiikharrr@users.noreply.github.com>
2026-04-20 11:55:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-//your JS code here. If required.
 const galleryImages = document.querySelectorAll(".gallery-img");
-const modalImage = document.getElementById("modalImage");
+const lightbox = document.querySelector(".lightbox");
 
 galleryImages.forEach((img) => {
   img.addEventListener("click", () => {
-    modalImage.src = img.src;
-    modalImage.alt = img.alt;
+    lightbox.style.backgroundImage = `url(${img.src})`;
   });
 });
